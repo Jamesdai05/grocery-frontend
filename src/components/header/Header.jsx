16ddf55f2f48a1c4197beeb0707e766e3df5855b
jamesdai05 <dai_guilong@hotmail.com>
2025-03-14 12:22:44 +0800

    add the link to the logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,18 @@
 import logo from "/images/logogrocery.png";
 import "./header.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="fixed shadow-md w-full h-20 px-2 header">
       <div className="">
-
-        <div className="">
-          <img src={logo} alt="logo" className="w-20"/>
-        </div>
+        <Link to="/">
+          <div className="flex justify-start items-center">
+            <img src={logo} alt="logo" className="w-20" />
+          </div>
+        </Link>
       </div>
-
     </header>
-  )
+  );
 }
 export default Header
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+const router=createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+
+    </Route>
+  )
 )
+
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
+  </StrictMode>
+);
