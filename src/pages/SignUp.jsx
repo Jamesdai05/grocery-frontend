1bbbcb37b6e203bec7a15b9b94ebbbdd91364432
jamesdai05 <dai_guilong@hotmail.com>
2025-03-17 13:20:28 +0800

    fixed the css issue of the form

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -6,8 +6,10 @@ const SignUp = () => {
   const [isPasswordShow, setIsPasswordShow] = useState(false);
   const [isConfirmPasswordShow, setIsConfirmPasswordShow] = useState(false);
 
+  const handleConfirmPassword=()=>setIsConfirmPasswordShow(prev=>!prev)
+
   return (
-    <div className="p-3 md:p-4">
+    <div className="flex justp-3 md:p-4">
       <div className="w-full max-w-sm bg-white m-auto flex items-center flex-col p-4">
         <div className="text-center w-20 overflow-hidden rounded-full drop-shadow-md">
           <img src={photo} alt="user" className="w-full" />
@@ -16,51 +18,49 @@ const SignUp = () => {
           <label htmlFor="firstName">First Name</label>
           <input
             type="text"
-            placeholder="enter your last name"
-            className="mt-1 w-full bg-slate-300 p-1 my-1 py-1 px-2 focus:outline-blue-300"
+            placeholder="enter your first name"
+            className="mt-1 w-full bg-slate-300 rounded-md p-1 my-1 py-1 px-2 outline-0 focus-within:outline focus-within:outline-blue-700 transition-colors"
             id="firstName"
           />
           <label htmlFor="lastName">Last Name</label>
           <input
             type="text"
-            placeholder="enter your first name"
+            placeholder="enter your last name"
             id="lastName"
-            className="w-full bg-slate-300 p-1 my-1 py-1 px-2 focus:outline-blue-300"
+            className="w-full bg-slate-300 rounded-md p-1 my-1 py-1 px-2 outline-0 focus-within:outline focus-within:outline-blue-700 transition-colors"
           />
           <label htmlFor="email">Email</label>
           <input
             type="email"
             placeholder="enter your email"
             id="email"
-            className="w-full bg-slate-300 p-1 my-1 py-1 px-2 focus:outline-blue-300"
+            className="w-full bg-slate-300 rounded-md p-1 my-1 py-1 px-2 outline-0 focus-within:outline focus-within:outline-blue-700 transition-colors"
           />
           <label htmlFor="password">Password</label>
-          <div className="flex justify-between items-center focus:border-blue-300">
+          <div className="flex items-center bg-slate-300 rounded-md focus-within:outline focus-within:outline-blue-700 transition-colors">
             <input
-              type="password"
+              type={isPasswordShow ? "text" : "password"}
               placeholder="enter your password"
               id="password"
-              className="outline-none border-none w-full bg-slate-300 p-1 my-1 py-1 px-2 focus:outline-blue-300"
+              className="w-full my-1 px-2 py-1 bg-slate-300 focus:outline-none rounded-l-md"
             />
             <span
               onClick={() => setIsPasswordShow((prev) => !prev)}
-              className="bg-slate-300 p-2"
+              className="bg-slate-300 px-2 py-2 rounded-r-md"
+            
             >
+
               {!isPasswordShow ? <FaRegEye /> : <FaRegEyeSlash />}
             </span>
           </div>
           <label htmlFor="confirmPassword">Confirm Password</label>
-          <div className="flex justify-between items-center focus:border-blue-300">
+          <div className="flex items-center bg-slate-300 rounded-md focus-within:outline focus-within:outline-blue-700 transition-colors">
             <input
-              type="confirmPassword"
-              placeholder="confirm your password"
-              id="password"
-              className="outline-none border-none w-full bg-slate-300 p-1 my-1 py-1 px-2 focus:outline-blue-300"
+              type={isConfirmPasswordShow ? "text" : "password"}
+              className="w-full my-1 px-2 py-1 bg-slate-300 focus:outline-none rounded-l-md"
+              placeholder="confirm the password"
             />
-            <span
-              onClick={() => setIsConfirmPasswordShow((prev) => !prev)}
-              className="bg-slate-300 p-2"
-            >
+            <span className="bg-slate-300 px-2 py-2 rounded-r-md" onClick={handleConfirmPassword}>
               {!isConfirmPasswordShow ? <FaRegEye /> : <FaRegEyeSlash />}
             </span>
           </div>
