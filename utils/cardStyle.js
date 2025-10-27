0d048fd103d3f6bf522208314251126a74a7af26
ThePrimeagen <dai_guilong@hotmail.com>
2025-10-28 01:15:56 +0800

    add the style for the card

---
+++ utils/cardStyle.js
@@ -0,0 +1,12 @@
+export const cardElementOptions = {
+    style: {
+        base: {
+            fontSize: "16px",
+            padding: "0.5em 1em",
+            color: "#1f2937",
+            "::placeholder": { color: "#9ca3af", fontSize:"1em"},
+            fontFamily: "system-ui, sans-serif",
+        },
+        invalid: { color: "#ef4444" },
+    },
+};
