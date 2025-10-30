58a1a740eca6233ce774decaa78cd492bd3fbe9b
ThePrimeagen <dai_guilong@hotmail.com>
2025-10-31 01:36:54 +0800

    feat:add the cart in the productcard component

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,26 @@
 import Rating from "./Rating";
 import { Link } from "react-router-dom";
+import { FaCartPlus } from "react-icons/fa";
+import { useCart } from "../hooks/useCart.js";
 
 const ProductCard = ({_id,image,name,numReviews,price,rating}) => {
+
+    const {addToCart}=useCart();
+    const product={
+        _id,
+        image,
+        rating,
+        numReviews,
+        name,
+        price
+    };
+
+    const handleAddToCart=()=>{
+        if(product){
+            addToCart(product,1);
+        }
+    }
+
   return (
       <div className="product px-2 bg-gray-100 border-2 rounded border-gray-300">
           <Link to={`/product/${_id}`}>
@@ -18,7 +37,14 @@ const ProductCard = ({_id,image,name,numReviews,price,rating}) => {
                   text={`${numReviews} Reviews`}
                   className="mb-3"
               />
-              <h4 className="text-2xl my-2 font-[500] px-2">${price.toFixed(2)}</h4>
+              <div className="add-cart">
+                  <h4 className="text-2xl my-2 font-[500] px-2">
+                      ${price.toFixed(2)}
+                  </h4>
+                  <button className="cart2" onClick={handleAddToCart}>
+                      <FaCartPlus />
+                  </button>
+              </div>
           </div>
       </div>
   );
